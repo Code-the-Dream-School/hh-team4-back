a9cec7bf303ba7b5bcaa88039fa6dc41abb5ea81
Tristan White <113216091+up2code3@users.noreply.github.com>
2024-12-09 20:04:03 -0500

    fixed

--- src/routes/medicationRoutes.js
+++ src/routes/medicationRoutes.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const authenticate = require("../middleware/authMiddleware");
 
-
 const roleMiddleware = require("../middleware/roleMiddleware");
-
 
 const {
   getMedications,
